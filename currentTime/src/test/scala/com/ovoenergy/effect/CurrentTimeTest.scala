551fb570d104d3b2e6c9b7f5b5607d5d94f21cf9
tomverran <tom.verran@ovoenergy.com>
2018-07-20 12:21:11 +0100

    Add a fixed instance of CurrentTime, add some tests

---
+++ currentTime/src/test/scala/com/ovoenergy/effect/CurrentTimeTest.scala
@@ -0,0 +1,42 @@
+package com.ovoenergy.effect
+
+import java.time.{Instant, ZoneId, ZonedDateTime}
+
+import cats.Id
+import cats.effect.IO
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{Matchers, WordSpec}
+
+class CurrentTimeTest extends WordSpec with Matchers with GeneratorDrivenPropertyChecks {
+
+  val Z: ZoneId = ZoneId.of("Z")
+  implicit val order: Ordering[ZonedDateTime] = _.compareTo(_)
+
+  "CurrentTime instance" should {
+    "Return the given time in UTC when using the fixed instance" in {
+      forAll { long: Long =>
+        val time = Instant.ofEpochMilli(long)
+        CurrentTime.fixed[Id](time).now shouldEqual time.atZone(Z)
+      }
+    }
+  }
+
+  "CurrentTime sync instance" should {
+
+    "Return a time in UTC" in {
+      CurrentTime.syncClock[IO].now.unsafeRunSync.getZone shouldBe Z
+    }
+
+    "Return increasing times" in {
+      val instance = CurrentTime.syncClock[IO]
+
+      val times = for {
+        time1 <- instance.now
+        time2 <- instance.now
+      } yield (time1, time2)
+
+      val (time1, time2) = times.unsafeRunSync
+      time1 should be < time2
+    }
+  }
+}
