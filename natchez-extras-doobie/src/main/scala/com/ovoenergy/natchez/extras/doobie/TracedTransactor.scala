98fca3e05648e820354df39d70700ac250e1612e
Elias Court <elias@k1nd.ltd>
2022-03-04 10:23:48 +0000

    Make the doobie code slightly more flexible/configurable

--- natchez-extras-doobie/src/main/scala/com/ovoenergy/natchez/extras/doobie/TracedTransactor.scala
+++ natchez-extras-doobie/src/main/scala/com/ovoenergy/natchez/extras/doobie/TracedTransactor.scala
@@ -2,6 +2,8 @@ package com.ovoenergy.natchez.extras.doobie
 
 import cats.data.Kleisli
 import cats.effect.Async
+import cats.implicits.catsSyntaxFlatMapOps
+import com.ovoenergy.natchez.extras.doobie.Config.ServiceAndResource
 import doobie.WeakAsync
 import doobie.free.KleisliInterpreter
 import doobie.util.transactor.Transactor
@@ -10,63 +12,69 @@ import natchez.{Span, Trace}
 import java.sql.{Connection, PreparedStatement, ResultSet}
 
 object TracedTransactor {
-
   type Traced[F[_], A] = Kleisli[F, Span[F], A]
+  def apply[F[_]: Async](
+    service: String,
+    transactor: Transactor[F]
+  ): Transactor[Traced[F, *]] = {
+    val kleisliTransactor = transactor
+      .mapK(Kleisli.liftK[F, Span[F]])(implicitly, Async.asyncForKleisli(implicitly))
+    trace(ServiceAndResource(s"$service-db", Config.DefaultResourceName), kleisliTransactor)
+  }
 
   private def formatQuery(q: String): String =
     q.replace("\n", " ").replaceAll("\\s+", " ").trim()
 
-  def apply[F[_]: Async](
-    service: String,
+  def trace[F[_]: Trace: Async](
+    config: Config,
     transactor: Transactor[F]
-  ): Transactor[Traced[F, *]] =
+  ): Transactor[F] =
     transactor
-      .mapK(Kleisli.liftK[F, Span[F]])(implicitly, Async.asyncForKleisli(implicitly))
       .copy(
-        interpret0 = new KleisliInterpreter[Traced[F, *]] {
-
-          implicit val asyncM: WeakAsync[Traced[F, *]] =
-            WeakAsync.doobieWeakAsyncForAsync(Async.asyncForKleisli[F, Span[F]])
-
-          val trace: Trace[Traced[F, *]] =
-            Trace.kleisliInstance
-
-          override lazy val PreparedStatementInterpreter: PreparedStatementInterpreter =
-            new PreparedStatementInterpreter {
-
-              type TracedOp[A] = Kleisli[Traced[F, *], PreparedStatement, A]
-
-              def runTraced[A](f: TracedOp[A]): TracedOp[A] =
-                Kleisli {
-                  case TracedStatement(p, sql) =>
-                    trace.span(s"$service-db:db.execute:${formatQuery(sql)}")(
-                      trace.put("span.type" -> "db").flatMap(_ => f(p))
-                    )
-                  case a =>
-                    f(a)
-                }
-
-              override val executeBatch: TracedOp[Array[Int]] =
-                runTraced(super.executeBatch)
-
-              override val executeLargeBatch: TracedOp[Array[Long]] =
-                runTraced(super.executeLargeBatch)
-
-              override val execute: TracedOp[Boolean] =
-                runTraced(super.execute)
-
-              override val executeUpdate: TracedOp[Int] =
-                runTraced(super.executeUpdate)
-
-              override val executeQuery: TracedOp[ResultSet] =
-                runTraced(super.executeQuery)
-            }
-
-          override lazy val ConnectionInterpreter: ConnectionInterpreter =
-            new ConnectionInterpreter {
-              override def prepareStatement(a: String): Kleisli[Traced[F, *], Connection, PreparedStatement] =
-                super.prepareStatement(a).map(TracedStatement(_, a): PreparedStatement)
-            }
-        }.ConnectionInterpreter
+        interpret0 = createInterpreter(config, Async[F]).ConnectionInterpreter
       )
+
+  private def createInterpreter[F[_]: Trace](config: Config, F: Async[F]): KleisliInterpreter[F] = {
+    new KleisliInterpreter[F] {
+      implicit val asyncM: WeakAsync[F] =
+        WeakAsync.doobieWeakAsyncForAsync(F)
+
+      override lazy val PreparedStatementInterpreter: PreparedStatementInterpreter =
+        new PreparedStatementInterpreter {
+
+          type TracedOp[A] = Kleisli[F, PreparedStatement, A] //PreparedStatement => F[A]
+
+          def runTraced[A](f: TracedOp[A]): TracedOp[A] =
+            Kleisli {
+              case TracedStatement(p, sql) =>
+                Trace[F].span(config.fullyQualifiedSpanName(formatQuery(sql)))(
+                  Trace[F].put("span.type" -> "db") >> f(p)
+                )
+              case a =>
+                f(a)
+            }
+
+          override val executeBatch: TracedOp[Array[Int]] =
+            runTraced(super.executeBatch)
+
+          override val executeLargeBatch: TracedOp[Array[Long]] =
+            runTraced(super.executeLargeBatch)
+
+          override val execute: TracedOp[Boolean] =
+            runTraced(super.execute)
+
+          override val executeUpdate: TracedOp[Int] =
+            runTraced(super.executeUpdate)
+
+          override val executeQuery: TracedOp[ResultSet] =
+            runTraced(super.executeQuery)
+        }
+
+      override lazy val ConnectionInterpreter: ConnectionInterpreter =
+        new ConnectionInterpreter {
+          override def prepareStatement(a: String): Kleisli[F, Connection, PreparedStatement] =
+            super.prepareStatement(a).map(TracedStatement(_, a): PreparedStatement)
+        }
+    }
+  }
 }
